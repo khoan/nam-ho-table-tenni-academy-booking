ef3e2cc5d1dd1d5856abbdcc31b5025163cff581
Khoa Nguyen <khoa@sense.capital>
2020-09-13 20:59:30 +1000

    pass through tachyons.css and banner.jpg

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("redom.js");
   eleventyConfig.addPassthroughCopy("sessionPickerApp.js");
+  eleventyConfig.addPassthroughCopy("tachyons.css");
+  eleventyConfig.addPassthroughCopy("banner.jpg");
 };
